24540bfc76bb64fe4abd93c68ff9cb391077afd0
APARNA RAJPUT <112623706+AR-30@users.noreply.github.com>
2023-12-07 15:42:49 +0530

    Update printFirstNprimeNo.py

--- printFirstNprimeNo.py
+++ printFirstNprimeNo.py
@@ -1,14 +1,14 @@
 ### printing first n prime numbers
 n=int(input("Enter number "))
-k=0
+i=0
 j=2
 print(f"First {n} prime numbers are:", end=" ")
-while(k!=n):
+while(i!=n):
     value=0
-    for i in range(2,j):
-        if(j%i==0):
+    for k in range(2,j):
+        if(j%k==0):
             value=1
     if(value==0):
         print(j, end=", ")
-        k+=1
+        i+=1
     j+=1
